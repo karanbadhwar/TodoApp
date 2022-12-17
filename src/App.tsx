717a0c831e0269dc8e916d8ac848c7f02f82e203
karanbadhwar <karanbadhwar07@gmail.com>
2022-12-17 09:48:00 -0500

    Updated Readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React, {FunctionComponent, ReactElement} from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './themes/customTheme';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 const App: FunctionComponent = (): ReactElement =>{
-  return <h1>Hello World</h1>
+  return (
+
+    <ThemeProvider theme={customTheme}>
+      {/* CssBaseline is to override the basic Css Files */}
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
 export default App;
