a3618cb7e40f00a42897fe7ba5d3db5ceb3f647e
karanbadhwar <karanbadhwar07@gmail.com>
2022-12-17 09:44:00 -0500

    Pages Direcotry with DashBoard Component

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,14 @@
+import React, {FC, ReactElement} from "react";
+import { Grid } from "@mui/material";
+import { Taskarea } from "../../components/taskarea/taskarea";
+import { Sidebar } from "../../components/sidebar/sidebar";
+
+export const Dashboard: FC = ():ReactElement =>{
+     return (
+        <Grid container minHeight="100vh" p={0} m={0} >
+            <Taskarea />
+            <Sidebar />
+            
+        </Grid>
+     )
+}
