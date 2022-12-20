a01d0a01b0272ce06a5d2d0fc821bda6ee8d7fa4
karanbadhwar <karanbadhwar07@gmail.com>
2022-12-20 13:32:25 -0500

    All missed Commits

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -3,7 +3,7 @@ import { Avatar, Box, Typography } from '@mui/material';
 // import PropTypes from 'prop-types';
 
 interface IProfile {
-  name?: string;
+  name: string;
 }
 
 export const Profile: FC<IProfile> = (props): ReactElement => {
@@ -38,5 +38,5 @@ export const Profile: FC<IProfile> = (props): ReactElement => {
 };
 
 // Profile.propTypes = {
-//   name: PropTypes.string,
+//   name: PropTypes.string.isRequired,
 // };
