429fba7255ed554af8d8abbe0ebe7cef85a13638
karanbadhwar <karanbadhwar07@gmail.com>
2022-12-20 13:52:21 -0500

    Adding propTypes to TaskField & TaskDescription

--- src/components/createTaskForm/_taskDescription.tsx
+++ src/components/createTaskForm/_taskDescription.tsx
@@ -1,6 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import { TextField } from '@mui/material';
 import { ITextField } from './interfaces/ITextField';
+import PropTypes from 'prop-types';
 
 export const TaskDescription: FC<ITextField> = (props): ReactElement => {
   const { onChange = (e) => console.log(e), disabled = false } = props;
@@ -19,4 +20,9 @@ export const TaskDescription: FC<ITextField> = (props): ReactElement => {
       onChange={onChange}
     />
   );
+};
+
+TaskDescription.propTypes = {
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
 };
