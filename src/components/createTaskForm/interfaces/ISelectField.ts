8cdbc97ead917fa3a3a5683f3dadfebc5066c8bf
karanbadhwar <karanbadhwar07@gmail.com>
2022-12-21 13:22:11 -0500

    Inteface Created for SelectForm Component

---
+++ src/components/createTaskForm/interfaces/ISelectField.ts
@@ -0,0 +1,15 @@
+import { SelectChangeEvent } from '@mui/material';
+import { IDIsabled } from './IDisabled';
+
+export interface ISelectField extends IDIsabled {
+  onChnage?: (e: SelectChangeEvent) => void;
+  name?: string;
+  value?: string;
+  label?: string;
+  items: ISelectItems[];
+}
+
+export interface ISelectItems {
+  value: string;
+  label: string;
+}
