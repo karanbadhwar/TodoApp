0adf6a2d5b90bb945fcb2233e0fc57fc76806897
karanbadhwar <karanbadhwar07@gmail.com>
2023-01-09 15:40:34 -0500

    Using React-Query Mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React, {FunctionComponent, ReactElement} from 'react';
+import React, { FunctionComponent, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './themes/customTheme';
 import { Dashboard } from './pages/dashboard/dashboard';
 
-const App: FunctionComponent = (): ReactElement =>{
-  return (
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-    <ThemeProvider theme={customTheme}>
-      {/* CssBaseline is to override the basic Css Files */}
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
-  )
-}
+const queryClient = new QueryClient();
+
+const App: FunctionComponent = (): ReactElement => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        {/* CssBaseline is to override the basic Css Files */}
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
 export default App;
--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -1,10 +1,20 @@
 import React, { FC, ReactElement, useState } from 'react';
-import { Box, Typography, Stack } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Stack,
+  LinearProgress,
+  Button,
+  Alert,
+  AlertTitle,
+} from '@mui/material';
+import { useMutation } from '@tanstack/react-query';
 
 import { TaskTitleField } from './_taskTitleField';
 import { TaskDescription } from './_taskDescription';
 import { TaskDateField } from './_taskDateField';
 import { TaskSelectorField } from './_taskSelectorField';
+import { sendApiRequest } from '../../helpers/sendApiRequest';
 
 import { Status } from './enums/Status';
 import { Priority } from './enums/Priority';
@@ -12,14 +22,15 @@ import { Priority } from './enums/Priority';
 export const CreateTaskForm: FC = (): ReactElement => {
   //Decalring State
   const [title, setTitle] = useState<string | undefined>(undefined);
-
   const [description, setDescription] = useState<string | undefined>(undefined);
-
   const [date, setDate] = useState<Date | null>(new Date());
-
   const [status, setStatus] = useState<string>(Status.todo);
-
   const [priority, setPriority] = useState<string>(Priority.normal);
+
+  // Create a Task Mutation
+  const createTaskMutation = useMutation((data) =>
+    sendApiRequest('http://localhost:3200/tasks', 'POST', data),
+  );
 
   return (
     <Box
@@ -30,17 +41,23 @@ export const CreateTaskForm: FC = (): ReactElement => {
       px={4}
       my={6}
     >
+      <Alert severity="success" sx={{ width: '100%', marginBottom: '16px' }}>
+        <AlertTitle>Success</AlertTitle>
+        The Task has been created Successfully
+      </Alert>
       <Typography mb={2} component="h2" variant="h6">
         Create A Task
       </Typography>
       <Stack spacing={2} sx={{ width: '100%' }}>
-        <TaskTitleField />
-        <TaskDescription />
-        <TaskDateField />
+        <TaskTitleField onChange={(e) => setTitle(e.target.value)} />
+        <TaskDescription onChange={(e) => setDescription(e.target.value)} />
+        <TaskDateField value={date} onChange={(date) => setDate(date)} />
         <Stack spacing={2} sx={{ width: '100%' }} direction="row">
           <TaskSelectorField
             label="Status"
             name="status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value as string)}
             items={[
               {
                 value: Status.todo,
@@ -55,6 +72,8 @@ export const CreateTaskForm: FC = (): ReactElement => {
           <TaskSelectorField
             label="Priority"
             name="priority"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as string)}
             items={[
               {
                 value: Priority.high,
@@ -71,6 +90,10 @@ export const CreateTaskForm: FC = (): ReactElement => {
             ]}
           />
         </Stack>
+        <LinearProgress />
+        <Button variant="contained" size="large" fullWidth>
+          Crerate A Task
+        </Button>
       </Stack>
     </Box>
   );
