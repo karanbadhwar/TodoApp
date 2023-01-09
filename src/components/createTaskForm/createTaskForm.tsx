f25308ad2f5f4b3a8b76efd9b530d68848220c41
karanbadhwar <karanbadhwar07@gmail.com>
2023-01-09 11:45:25 -0500

    Added State

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import { Box, Typography, Stack } from '@mui/material';
 
 import { TaskTitleField } from './_taskTitleField';
@@ -10,6 +10,17 @@ import { Status } from './enums/Status';
 import { Priority } from './enums/Priority';
 
 export const CreateTaskForm: FC = (): ReactElement => {
+  //Decalring State
+  const [title, setTitle] = useState<string | undefined>(undefined);
+
+  const [description, setDescription] = useState<string | undefined>(undefined);
+
+  const [date, setDate] = useState<Date | null>(new Date());
+
+  const [status, setStatus] = useState<string>(Status.todo);
+
+  const [priority, setPriority] = useState<string>(Priority.normal);
+
   return (
     <Box
       display="flex"
